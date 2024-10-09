2a15562a45454f32167b9a472e5c0a06e7e7eb28
ricardowrld <160732742+ricardowrld@users.noreply.github.com>
2024-10-09 09:13:29 -0300

    a

--- ex5/script.js
+++ ex5/script.js
@@ -7,15 +7,15 @@ frm.addEventListener("submit", (e) => {
 
     IMC = (peso / altura) ** 2
 
-    if (IMC <= 18,5) {
+    if (IMC <= 18.5) {
         saida.innerText = `Baixo Peso`
-    } else if (IMC >=18,6 && IMC <= 24,9) {
+    } else if (IMC >=18.6 && IMC <= 24.9) {
         saida.innerText = `Peso Normal`
-    } else if (IMC >= 25 && IMC <= 29,9) {
+    } else if (IMC >= 25 && IMC <= 29.9) {
         saida.innerText = `Sobrepeso`
-    } else if (IMC >= 30 && IMC <= 34,9) {
+    } else if (IMC >= 30 && IMC <= 34.9) {
         saida.innerText = `Obesidade grau 1`
-    } else if (IMC >= 35 && IMC <= 39,9) {
+    } else if (IMC >= 35 && IMC <= 39.9) {
         saida.innerText = `Obesidade grau 2`
     } else if (IMC >= 40) {
         saida.innerText = `Obesidade grau 3`
